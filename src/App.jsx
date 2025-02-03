69990374e14b36c115b091013a6135bc9a0d0c1d
Yasemin Ceylan <yaceybln@gmail.com>
2025-02-03 14:43:46 +0100

    added styling for calculator

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
